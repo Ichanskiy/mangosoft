13899f0914a61fbf1f84969a3139f40fcd6efd6c
Ichanskiy <ichanskiy.olexandr@gmail.com>
2018-05-18 17:13:29 +0300

    update registration path

--- src/main/java/com/mangosoft/controller/ControllerAPI.java
+++ src/main/java/com/mangosoft/controller/ControllerAPI.java
@@ -2,7 +2,7 @@ package com.mangosoft.controller;
 
 class ControllerAPI {
 
-    static final String REGISTRATION = "/registration";
+    static final String REGISTRATION = "/";
 
     static final String USER_CONTROLLER = "/user";
     static final String GENERAL_REQUEST = "";
